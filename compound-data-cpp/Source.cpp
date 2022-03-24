dfeaaf3a3f452e3ef40578f874ec4d3e9589cd52
Matthew Kirkendall <kirk.viola@gmail.com>
2022-03-24 14:30:11 -0400

    added pointers namespace

--- compound-data-cpp/Source.cpp
+++ compound-data-cpp/Source.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <array>
+#include <string>
 using namespace std;
 
 
@@ -20,11 +21,69 @@ namespace arrMethods {
 	}
 }
 
+namespace characterMethods {
+	// arrays of characters are ended with special \0 character
+
+	char myFirstName[] = { 'M', 'a', 't', 't', 'h', 'e', 'w', '\0' }; // Length includes the null character at end
+	char myLastName[] = "Kirkendall"; // null character included behind the scenes
+
+	string fullName = myLastName; // Can convert arrays, but not concat
+	
+	void printMessage() {
+		char message[80]; // Fixed size must be specified, unlike strings
+
+		cout << "What word would you like to say?"; 
+		cin >> message; // Remember! character arrays terminate with white space
+		cout << "Okay, I will say: " << message << endl;
+
+	}
+
+}
+
+namespace pointerOps {
+
+	 void get_int_address(int num) {
+		int* address = &num;
+		
+		cout << "Memory location is: " << address << endl;
+		cout << "Variable content is: " << *address << endl; // *'deference' sign points to variable value held at pointer
+	 }
+
+	 void array_pointer_demo(int ar[]) {
+
+		 cout << ar << endl;
+
+		 int* myPointer = ar; // Can implicitly convert arrays into pointers
+
+		 cout << myPointer << endl;
+
+		 // Arrays and pointers can do the same functions
+	 }
+
+	 void pointer_arithmatic(int num) {
+
+		 int* pointer = &num;
+
+		 cout << "Initial location: " << pointer << endl;
+		 cout << "New location: " << ++pointer << endl;
+	 }
+}
+
 int main() {
 
 	array<int, 4> sampleArray = { 2, 4, 6, 8 };
 
-	arrMethods::printArray(sampleArray);
+	arrMethods::printArray(sampleArray); // Calling upon namespace syntax, like a simple . in C#
+
+	//characterMethods::printMessage();
+
+	int testNum = 500;
+	int testAr[5] = { 10, 20, 30, 40, 50 };
+
+	pointerOps::get_int_address(testNum);
+	pointerOps::array_pointer_demo(testAr);
+	pointerOps::pointer_arithmatic(testNum);
+
 
 	return 0;
 }
